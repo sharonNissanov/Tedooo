17c86d1a170129c28a06e3ccbe8454d082edee45
Sharon <sharonn@envitechsoftware.com>
2023-03-23 17:09:02 +0200

    install and use fast-average-color

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,18 @@
+import { FastAverageColor } from 'fast-average-color';
+
 type ContentProps = {
     text: string,
     imgs: Array<string>
 }
+
+
+
+
  const Content = (props : ContentProps)=>{
+
+
+    const fac = new FastAverageColor();
+
 
     const NUMBER_OF_IMAGES = 2;
     let relevantImages :Array<string> =[];
@@ -13,13 +23,25 @@ type ContentProps = {
     //console.log(relevantImages, props.imgs,imgsLength)
     imgWidth  = relevantImages.length != 0 ? ( 100/relevantImages.length) + "%": "";
 
+function test(str:string): void{
+    fac.getColorAsync(str)
+    .then(color => {
+        console.log(color)
+    })
+    .catch(e => {
+        console.log(e);
+    });
+}
+
+    
+
     return (
         <div>
             <div id='text'>{props.text}</div>
             <div className="content-imgs-container">
 
                 {relevantImages.map((img) => (
-                    <img key={img} src={img} alt="Avatar" className="single-content-img" style={{width: imgWidth}}/>
+                    <img key={img} onLoad={()=>{test(img)}} src={img} alt="Avatar" className="single-content-img" style={{maxWidth: imgWidth}}/>
                 ))}
 
             </div>
