280c0008e293bfc1cf648c96f54c70aeb8f4f465
Sharon <sharonn@envitechsoftware.com>
2023-03-23 09:16:53 +0200

    API call

---
+++ src/utils/ApiHelper.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const getData = async () => {
+    try {
+
+        let response = await axios.get('https://dev.tedooo.com/feed.json')
+        console.log(response);
+        return response.data;
+    } catch(error){
+
+        console.error('There was an error!', error);
+        return [];
+    }
+  }
+
+  export default getData;
