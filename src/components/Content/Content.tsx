599d95066af9d4c2db2eba0ab11e290c805bb83d
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2023-03-24 12:12:38 +0300

    trying to save file for average color

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,47 +1,59 @@
 import { FastAverageColor } from 'fast-average-color';
-
+import {saveAs} from 'file-saver';
+import logo from '../../logo.svg';
 type ContentProps = {
     text: string,
     imgs: Array<string>
 }
 
-
-
-
  const Content = (props : ContentProps)=>{
-
-
-    const fac = new FastAverageColor();
-
-
     const NUMBER_OF_IMAGES = 2;
     let relevantImages :Array<string> =[];
     let imgWidth : string;
     let imgsLength : number = props.imgs.length;  
 
     relevantImages = props.imgs.slice(0, NUMBER_OF_IMAGES);
-    //console.log(relevantImages, props.imgs,imgsLength)
     imgWidth  = relevantImages.length != 0 ? ( 100/relevantImages.length) + "%": "";
 
-function test(str:string): void{
-    fac.getColorAsync(str)
-    .then(color => {
-        console.log(color)
-    })
-    .catch(e => {
-        console.log(e);
-    });
-}
+    function test3(e:any,){
+        console.log(e.target.src)
+        const file = new Blob ([e.target.src], {type:"jpg"})
+    //    saveAs(file, "sharon-"  + e.target.src);
+    }
+    // function test2(e:any){
+    //     const fac = new FastAverageColor();
+    //     let newImg :HTMLImageElement= document.createElement("img");
+    //     newImg.src = e.target.src;
+    //     newImg.crossOrigin ="anonymous";
+    //     //if(newImg !=null){
+    //       //  newImg.onload((e:any)=>{
+    //             // fac.getColorAsync(newImg)
+    //             // .then(color => {
+    //             //     console.log(color)
+    //             // })
+    //             // .catch(e => {
+    //             //     console.log(e);
+    //             // });    
+    //        // }) 
+    //   //  }
 
+    //   fac.getColorAsync(logo)
+    //   .then(color => {
+    //       console.log(color)
+    //   })
+    //   .catch(e => {
+    //       console.log(e);
+    //   });    
+
+    // }
     
 
     return (
         <div>
             <div id='text'>{props.text}</div>
             <div className="content-imgs-container">
-
                 {relevantImages.map((img) => (
-                    <img key={img} onLoad={()=>{test(img)}} src={img} alt="Avatar" className="single-content-img" style={{maxWidth: imgWidth}}/>
+                    <img  key={img} onLoad={test3} src={img} alt="Avatar" className="single-content-img" style={{maxWidth: imgWidth}}/>
                 ))}
 
             </div>
