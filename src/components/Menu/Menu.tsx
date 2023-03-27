5a5607c3d7e4c7644daa2e2dcd378556d8d15314
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2023-03-27 14:31:39 +0300

    handle app bar

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,121 @@
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+    
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
+import "./MenuStyle.css";
+import {HomeOutlined, ChatBubbleOutlineRounded, NotificationsOutlined} from '@mui/icons-material';
+import userImg from"../../images/user.png";
+
+import SearchAppBar from './Search'
+
+export default function MenuComponent() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <React.Fragment>
+        <div style={{ background:"white"}}> 
+        <Container sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', padding: '1% 0'}}>
+            <Box><SearchAppBar/></Box>   
+        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <MenuItem  selected= {true} sx={{color:"#47C1AD"}} onClick={handleClose}> <HomeOutlined/> Home </MenuItem>
+        <MenuItem sx={{color:"#A1A4A3"}} onClick={handleClose}> <ChatBubbleOutlineRounded /> Messaging </MenuItem>
+        <MenuItem sx={{color:"#A1A4A3 "}} onClick={handleClose}> <NotificationsOutlined /> Notification </MenuItem>
+
+            <Tooltip title="Account settings">
+            <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+            >
+                <Avatar src={userImg} sx={{ width: 32, height: 32 }}></Avatar>
+            </IconButton>
+            </Tooltip>
+        </Box>
+        <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+            elevation: 0,
+            sx: {
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                mt: 1.5,
+                '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+                },
+                '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+                },
+            },
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+            <MenuItem onClick={handleClose}>
+            <Avatar /> Profile
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+            <Avatar /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+                <PersonAdd fontSize="small" />
+            </ListItemIcon>
+            Add another account
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+                <Settings fontSize="small" />
+            </ListItemIcon>
+            Settings
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+                <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+            </MenuItem>
+        </Menu>
+
+      </Container>
+      </div>
+    </React.Fragment>
+  );
+}
---
+++ src/components/Menu/Search.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import appLogo from '../../images/appLogo.png'
+import Avatar from '@mui/material/Avatar';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: '20px',
+  border: '#A1A4A3 1px solid',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 , display:"flex"}}>
+        <Avatar src={appLogo} style={{borderRadius: '20%'}}> </Avatar>
+        <Search>
+        <SearchIconWrapper>
+            <SearchIcon  style={{color:"#A1A4A3"}}/>
+        </SearchIconWrapper>
+        <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+        />
+        </Search>
+    </Box>
+  );
+}
