3e254fcf9cfab8ca78d0e9e78d1855fa4a14c18c
sharonNissanov <58398243+sharonNissanov@users.noreply.github.com>
2023-03-27 16:26:48 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import User from './components/User';
 import Content from './components/Content';
 import ButtonsBar from './components/LikeAndComment';
 import getData from './utils/ApiHelper';
+import testDate from './utils/Test';
+import { Card } from '@mui/material';
+import MenuComponent from './components/Menu'
 
 function App() {
   const [response, setResponse] = useState<any>([]);
@@ -16,7 +19,8 @@ function App() {
   const fetchData = async () => {
     let res = await getData();
     setResponse(res);
-    setResponseNew(res.slice(0,6));
+    setResponseNew(res.slice(0, FEED_ITEMS ));
+    //testDate()
   };
   fetchData();
 }, []);
@@ -33,20 +37,24 @@ function trackScrolling():void{
 
   return (
     <div className="App">
+      <MenuComponent/>
       {
       responseNew.map((data: any, index: number) => (
       
-        <div key={index} className='feed-data-item' >
+        <Card key={index} className='feed-data-item' >
           <User username = {data.username} 
           userId= {data.userId} 
           avatar={data.avatar} 
           shopName = {data.shopName}
-          shopId =  {data.shopId}/>
+          shopId =  {data.shopId}
+          premium =  {data.premium}
+          date =  {data.date}
+          />
 
           <Content text = {data.text} imgs = {data.images} />
 
           <ButtonsBar  didLike={data.didLike}  likes ={data.likes} comments ={data.comments}/>
-      </div>
+      </Card>
 
       ))} 
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,8 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  //<React.StrictMode>
+//<React.StrictMode>
+
     <App  />
   //</React.StrictMode>
 );
